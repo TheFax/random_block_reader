4dc73bb8a2c50791007b5700cd2cbf45f46c9881
TheFax <fabioslurp@yahoo.it>
2018-12-31 13:26:07 +0100

    Update random_block_reader.sh
    This code should be revised

--- random_block_reader.sh
+++ random_block_reader.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+#usage:   random_block_reader device
+#example: random_block_reader /dev/sda
 
+#retrieve the size of the block device
 bsize=$(blockdev --getsize $1)
 
 echo -e "The total size of " $1 " is " $bsize " blocks."
 
+#forever:
 while (true); do
-    bread=$RANDOM
-    bigrandom=$(((RANDOM<<15)|RANDOM))  #30 bit random
+    bread=$RANDOM   #How many sector should we read? 
+    bigrandom=$(((RANDOM<<15)|RANDOM))  #The starting sector (30 bit random)
     let "bmax=bsize-bread"
     let "brandom=bigrandom%bmax"
     echo -e "Reading " $bread " blocks starting from " $brandom 
